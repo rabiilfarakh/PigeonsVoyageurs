dcde1fac4117b8f590c3508263216cf2faf3b5f1
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-10-28 11:05:19 +0100

    add project

---
+++ src/main/java/com/example/PigeonsVoyageurs/PigeonsVoyageursApplication.java
@@ -0,0 +1,14 @@
+package com.example.PigeonsVoyageurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonsVoyageursApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PigeonsVoyageursApplication.class, args);
+	}
+
+}
