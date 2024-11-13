9f0f4e07b624d0bd2f741eeee1e6640afa5288c8
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-11-13 11:03:13 +0100

    PIG-20 Mettre en place le crud de Season

--- src/main/java/com/example/PigeonsVoyageurs/season/service/SeasonService.java
+++ src/main/java/com/example/PigeonsVoyageurs/season/service/SeasonService.java
@@ -1,9 +1,11 @@
 package com.example.PigeonsVoyageurs.season.service;
 
+import com.example.PigeonsVoyageurs.season.Season;
 import com.example.PigeonsVoyageurs.season.dto.SeasonRequestDTO;
 import com.example.PigeonsVoyageurs.season.dto.SeasonResponseDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SeasonService {
     SeasonResponseDTO save(SeasonRequestDTO seasonDTO);
@@ -13,4 +15,6 @@ public interface SeasonService {
     List<SeasonResponseDTO> findAll();
 
     void delete(String id);
+
+    public Optional<Season> findByName(String name);
 }
